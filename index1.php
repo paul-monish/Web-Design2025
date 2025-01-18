9ac7c5f9163f8b594d4171378097f4e6ab11448d
paul-monish <monish.paul2000@gmail.com>
2025-01-18 22:31:40 +0530

    init setup

---
+++ index1.php
@@ -0,0 +1,15 @@
+<?php
+// echo "hello world";
+// echo "hello"," ","world";
+// print "Hello world1"; 
+/*
+
+*/
+// $res=print("Hello world2");
+// echo $res;//1
+
+$x=5;
+$y=10;
+$c=$x+$y;
+echo "Addition of ",$x," and ",$y," is: ",$c;
+?>
