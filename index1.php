b086dc720ad1a61b33a9c99ae2940f26a532b332
paul-monish <monish.paul2000@gmail.com>
2025-03-08 23:53:33 +0530

    second commit

--- index1.php
+++ index1.php
@@ -123,11 +123,273 @@
 // echo "Outside of the function:$n";//5
 
 //pass by reference
-function increment(&$n){
-    $n++;
-    echo "Inside function:$n\n";//6
+// function increment(&$n){
+//     $n++;
+//     echo "Inside function:$n\n";//6
+// }
+// $n=5;
+// increment($n);
+// echo "Outside of the function:$n";//6
+
+
+// $a=5;
+// $a=(string)$a;
+// $a=(float)$a;
+// // $a=(bool)$a;
+// // $a=array($a);
+// // var_dump($a);
+// // $a=(object)$a;
+// $a=(unset)$a;
+// print_r($a);
+
+// echo gettype($a);
+
+
+// echo(pi());
+// echo(min(5,1,2));
+// echo(max(5,1,3));
+// echo(abs(-6.4));
+// echo(sqrt(64));
+// echo(round(0.60));
+// echo(rand(10,100));
+
+//
+//     const SALUTATION1="MR.";
+
+// const SALUTATION1="MR.";//
+// // echo SALUTATION1;
+// function test(){
+//     // define('SALUTATION','MR.');
+//     echo SALUTATION;
+// }
+// test();
+
+// $a=5;
+// $b=5;
+// echo($a===$b?'true':'false');
+
+
+// $a='hello' . 'world';
+// echo $a;
+
+// $a='hello';
+// $a.='world';
+// echo $a;
+
+// $arr1=array("0"=>"a","1"=>"b");
+// $arr2=array("2"=>"c","3"=>"d");
+// $arr3=$arr1+$arr2;
+// print_r($arr3);
+
+// $a=NULL;
+// $b=6;
+
+// echo $a??$b;
+
+
+// if(condition){
+
+// }
+// elseif(condition){
+
+// }
+// else{
+
+// }
+
+// if(condition){
+//     if(condition){
+
+//     }
+// }
+
+
+// $a=2;
+// if($a%2===0){
+//     $c=$a*5;
+//     if($c>7){
+//         echo "SOMETHING";
+//     }
+//     else{
+//         echo "NOTHING";
+//     }
+// }
+// else{
+//     echo "odd";
+// }
+
+
+// switch(expression){
+//     case '1':
+//         //
+//         break;
+//     case '2':
+//         //
+//         break;
+//     default:
+//     //
+// }
+
+// $a=5;
+
+// switch($a%2){
+//     case 0:
+//         echo "even";
+//         break;
+//     case 1:
+//         echo "odd";
+//         break;
+// }
+
+
+$name="Rajesh";
+
+// $i=0;
+// while($i<5){
+//     echo $name."<br/>";
+//     $i++;
+// }
+
+// $i=0;
+// do{
+//     echo "$name<br/>";
+//     $i++;
+// }while($i>5);
+
+// for($i=0;$i<5;$i++){
+//     echo "$name<br/>";
+// }
+
+// for($i=0;$i<5;$i++){
+//     echo $i."<br/>";
+//     if($i==4) break;
+// }
+
+// for($i=0;$i<5;$i++){
+//     if($i==3) continue;
+//     echo $i."<br/>";
+// }
+
+
+// $arr=array(1,2,3,4,6,8,9);
+// $len=count($arr);
+// echo $len;
+// for($i=0;$i<$len;$i++){
+//     echo $arr[$i]."<br/>";
+// }
+// foreach($arr as $e){
+//     echo $e."<br/>";
+// }
+
+// echo $arr[3];
+
+// $arr=array(
+//     "name"=>"Rajesh Manna",
+//     "Roll"=>54,
+// );
+// // echo $arr1["Roll"];
+
+// foreach($arr as $key=>$val){
+//     echo $key." ".$val."<br/>";
+// }
+
+// $arr=array(
+//     array(1,2,3),
+//     array(4,5,6),
+//     array(7,8,9),
+// );
+// echo $arr[1][1];
+
+// $arr=array(
+//     "1st"=>array(1,2,3),
+//     "2nd"=>array(4,5,6),
+//     "3rd"=>array(7,8,9),
+// );
+// // echo $arr["2nd"][1];
+
+// foreach($arr as $key=>$val){
+//     foreach($val as $v){
+//         echo $v." ";
+//     }
+//     echo "<br/>";
+// }
+
+// $arr=array("a","c","e","b");
+// sort($arr);
+// rsort($arr);
+
+// $arr=array("4"=>"b","1"=>"a","3"=>"d","2"=>"c");
+// asort($arr);
+// arsort($arr);
+// krsort($arr);
+// print_r($arr);
+
+// $arr=array(
+//     "1st"=>array("1"=>1,"2"=>2,"3"=>3),
+//     "2nd"=>array("4"=>4,"5"=>5,"6"=>6),
+//     "3rd"=>array("7"=>7,"8"=>8,"9"=>9),
+// );
+// echo $arr["2nd"]["5"];
+
+// $arr=array(
+//     array("1"=>1,"2"=>2,"3"=>3),
+//     array("4"=>4,"5"=>5,"6"=>6),
+//     array("7"=>7,"8"=>8,"9"=>9),
+// );
+// echo $arr[1]["5"];
+
+// $arr=array(8,-2,4,0,5,1,2);
+// array_push($arr,7,5);
+// array_pop($arr);
+// array_shift($arr);
+// array_unshift($arr,9);
+
+// function change1($v){
+//     return $v*2;
+// }
+// $new_arr=array_map("change1",$arr);
+// print_r($new_arr);
+
+// $arr=array("4"=>"b","1"=>"a","3"=>"d","2"=>"c");
+// $keys=array_keys($arr);
+// $values=array_values($arr);
+// print_r($keys);
+// print_r($values);
+
+// function bSort(&$arr){
+//     $n=count($arr);
+//     for($i=$n-1;$i>=0;$i--){
+//         for($j=0;$j<$i-1;$j++){
+//             if($arr[$j]>$arr[$j+1]){
+//                 $temp=$arr[$j+1];
+//                 $arr[$j+1]=$arr[$j];
+//                 $arr[$j]=$temp;
+//             }
+//         }
+//     }
+// }
+// bSort($arr);
+// print_r($arr);
+
+//super-global variable
+// $_POST,$_GET,$_REQUEST,$_SESSION,$_COOKIE,$_FILES,$GLOBALS
+
+
+$x=5;//$GLOBALS['x']=5;
+$y=6;//$GLOBALS['y']=6;
+
+// function add($a,$b){
+//     $c=$a+$b;
+//     // echo $c;
+//     return $c;
+// }
+
+function add(){
+    $z=$GLOBALS['x']+$GLOBALS['y'];
+    echo $z;
 }
-$n=5;
-increment($n);
-echo "Outside of the function:$n";//6
+// $z= add($x,$y);
+add();
+// echo $z;
+
 ?>
